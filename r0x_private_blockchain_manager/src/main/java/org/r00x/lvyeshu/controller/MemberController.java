18c699f913d8f2e6161ca19bf4caeb0d18086cbc
82049406@qq.com <82049406@qq.com>
2018-07-08 14:11:38 +0800

    tactx

--- r0x_private_blockchain_manager/src/main/java/org/r00x/lvyeshu/controller/MemberController.java
+++ r0x_private_blockchain_manager/src/main/java/org/r00x/lvyeshu/controller/MemberController.java
@@ -1,4 +1,8 @@
 package org.r00x.lvyeshu.controller;
 
+import javax.annotation.Resource;
+import javax.xml.ws.WebServiceRefs;
+
 public class MemberController {
+
 }
--- r0x_private_blockchain_manager/src/main/java/org/r00x/lvyeshu/manager/MemberManager.java
+++ r0x_private_blockchain_manager/src/main/java/org/r00x/lvyeshu/manager/MemberManager.java
@@ -57,6 +57,7 @@ public class MemberManager {
     return memberData;
   }
 
+
 }
 
 
